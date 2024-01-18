85f6b48c201f46a2fd8a88154a238a69eb90c18b
Joelnicolass <joelnicolass@gmail.com>
2024-01-18 09:39:54 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// tipos primitivos
+const numero = 1;
+const cadena = "string";
+const booleano = true;
+
+// objetos
+const objeto = {};
+const array = [];
+const funcion = () => {};
+
+let arr1 = ["nico"];
+let arr2 = ["nico"];
+
+console.log(arr2.includes("nico"));
+
+// los objetos se pasan por referencia!!!
